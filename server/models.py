0f25caed1c84253fcf875fb385149b56037f5b63
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-07-14 17:08:25 +0300

    changes

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
@@ -6,4 +7,12 @@ metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<Pet {self.id}, {self.magnitude}, {self.location},{self.year}>'
